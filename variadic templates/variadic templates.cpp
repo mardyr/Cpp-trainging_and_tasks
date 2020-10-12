a1e64f14aa41ebda5e6d6870b89e91aed80ac510
Marcin Dyrdół <marcindyrdol@gmail.com>
2020-10-12 22:26:52 +0200

    Add Variadic Templates file

---
+++ variadic templates/variadic templates.cpp
@@ -0,0 +1,32 @@
+﻿// variadic templates.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+template <typename T>
+std::string toStringInstance(const T& a)
+{
+    std::stringstream ss;
+    ss << a;
+    return ss.str();
+}
+template<typename ... Param>
+std::vector<std::string> toString(const Param& ... p)
+{
+    return { toStringInstance(p)... };
+}
+
+int main()
+{
+    const auto vec = toString("Martin", 111.2, 0, -9999, 1.4f);
+    
+    for (const auto& i : vec)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+    return 0;
+}
+
